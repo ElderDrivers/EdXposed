bc145584512eecc94c331c872f84e9dd17818c2c
LoveSy <shana@zju.edu.cn>
2021-02-03 02:30:44 +0800

    Lock critial section

--- edxp-core/src/main/cpp/main/src/jni/edxp_pending_hooks.cpp
+++ edxp-core/src/main/cpp/main/src/jni/edxp_pending_hooks.cpp
@@ -2,6 +2,7 @@
 #include <nativehelper/jni_macros.h>
 #include <string>
 #include <unordered_set>
+#include <shared_mutex>
 #include "HookMain.h"
 #include "jni.h"
 #include "native_util.h"
@@ -12,10 +13,13 @@
 namespace edxp {
 
     static std::unordered_set<const void *> pending_classes_;
+    static std::shared_mutex pending_classes_lock_;
 
     static std::unordered_set<const void *> hooked_methods_;
+    static std::shared_mutex hooked_methods_lock_;
 
     bool IsClassPending(void *clazz) {
+        std::shared_lock lk(pending_classes_lock_);
         return pending_classes_.count(clazz);
     }
 
@@ -24,6 +28,7 @@ namespace edxp {
         art::mirror::Class mirror_class(class_ptr);
         if (auto def = mirror_class.GetClassDef(); LIKELY(def)) {
             LOGD("record pending: %p (%s)", class_ptr, mirror_class.GetDescriptor().c_str());
+            std::unique_lock lk(pending_classes_lock_);
             pending_classes_.insert(def);
         } else {
             LOGW("fail to record pending for : %p (%s)", class_ptr,
@@ -40,10 +45,12 @@ namespace edxp {
     }
 
     bool isHooked(void *art_method) {
+        std::shared_lock lk(hooked_methods_lock_);
         return hooked_methods_.count(art_method);
     }
 
     void recordHooked(void *art_method) {
+        std::unique_lock lk(hooked_methods_lock_);
         hooked_methods_.insert(art_method);
     }
 
